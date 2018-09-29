98a6fab7fb36fb36c32920eeea56165f57c77e26
JaredCHunter07 <jared-9yearold@live.com>
2018-09-29 17:25:07 -0600

    Added back code for Subset. Sorry, Kyle.

--- CSIS-2420-A02/src/Randomized/Subset.java
+++ CSIS-2420-A02/src/Randomized/Subset.java
@@ -1,7 +1,20 @@
 package Randomized;
 
-public class Subset {
-	public static void main(String[] args) {
+import edu.princeton.cs.algs4.StdIn;
 
+public class Subset {
+
+	public static void main(String[] args) {
+		RandomizedQueue<String> rq = new RandomizedQueue<String>();
+		int k = Integer.valueOf(args[0]);
+		//System.out.println(k);
+		while (!StdIn.isEmpty()) {
+            rq.enqueue(StdIn.readString());
+        }
+		//System.out.println("test");
+		for (int i = 0; i < k; i++) {
+			
+			System.out.println(rq.dequeue());
+		}
 	}
 }
